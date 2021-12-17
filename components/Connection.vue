00536ebfab2ebc40776c84a392f77e4e406a994d
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2021-12-17 14:09:08 +0100

    pageConnection

---
+++ components/Connection.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="column is-8">
+    <div class="card">
+      <h1 class="has-text-centered is-size-4 pt-4">
+        <b>Connection</b>
+      </h1>
+      <section class="pb-4">
+        <b-field
+          label="Email:"
+          class="column is-6"
+          style="margin:0 auto;"
+        >
+          <b-input
+            type="email"
+            placeholder="entrez votre email"
+          />
+        </b-field>
+        <b-field
+          label="Mot de passe:"
+          class="column is-6"
+          style="margin:0 auto;"
+        >
+          <b-input
+            type="password"
+            placeholder="entrez votre mot de passe"
+          />
+        </b-field>
+        <div class="is-flex is-justify-content-center pb-4 pt-4">
+            <b-button type="is-primary" outlined>Connection</b-button>
+        </div>
+        <p class="has-text-centered">
+          Mot de passe oublié ?
+        </p>
+        <p class="has-text-centered">
+          Pas encore de compte ? <a>Creez en un</a>
+        </p>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnectionItem'
+}
+</script>
---
+++ pages/connexion/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <div class="columns is-multiline is-justify-content-center">
+      <Connection />
+    </div>
+  </section>
+</template>
+
+<script>
+import Connection from '../../components/Connection'
+export default {
+  name: 'IndexPage',
+  components: {
+    Connection
+  }
+}
+</script>
