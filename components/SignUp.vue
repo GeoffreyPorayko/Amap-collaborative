ebf1ae08f535d7d98586d0c9e98830befb464cbd
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2021-12-17 15:35:16 +0100

    pageSignUp

---
+++ components/SignUp.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="column is-8">
+    <div class="card">
+      <h1 class="has-text-centered is-size-4 pt-4">
+        <b>Inscription</b>
+      </h1>
+      <section class="pb-4 pl-6 pr-6 mt-4">
+        <div class="is-flex is-justify-content-center">
+          <b-field
+            label="Nom:"
+            class="column is-6"
+          >
+            <b-input
+              type="text"
+              placeholder="entrez votre nom"
+            />
+          </b-field>
+          <b-field
+            label="Prénom"
+            class="column is-6"
+          >
+            <b-input
+              type="text"
+              placeholder="entrez votre prénom"
+            />
+          </b-field>
+        </div>
+        <div class="is-flex is-justify-content-center">
+          <b-field
+            label="Email:"
+            class="column is-6"
+          >
+            <b-input
+              type="text"
+              placeholder="entrez votre email"
+            />
+          </b-field>
+          <b-field
+            label="Téléphone"
+            class="column is-6"
+          >
+            <b-input
+              type="text"
+              placeholder="entrez votre numéro de téléphone"
+            />
+          </b-field>
+        </div>
+        <div class="is-flex is-justify-content-center">
+          <b-field
+            label="Mot de passe:"
+            class="column is-6"
+          >
+            <b-input
+              type="password"
+              placeholder="entrez votre mot de passe"
+            />
+          </b-field>
+          <b-field
+            label="Confirmation du mot de passe"
+            class="column is-6"
+          >
+            <b-input
+              type="text"
+              placeholder="confirmez votre mot de passe"
+            />
+          </b-field>
+        </div>
+        <div class="is-flex is-justify-content-center">
+          Êtes-vous un expert en cuisine ? &nbsp;
+          <b-radio
+            v-model="radio"
+            name="expertRatio"
+            native-value="oui"
+            v-on:click.native="seen = true"
+          >
+            oui
+          </b-radio>
+          <b-radio
+            v-model="radio"
+            name="expertRatio"
+            native-value="non"
+            v-on:click.native="seen = false"
+            checked
+          >
+            non
+          </b-radio>
+        </div>
+        <div v-if="seen" id="expert" class="columns is-flex-wrap-wrap mt-4" style="border:solid lightgrey 1px;">
+          <b-field class="column is-6">
+            <b-checkbox id="cuissonFour" class="checkbox">
+              Cuisson au four
+            </b-checkbox>
+          </b-field>
+          <b-field class="column is-6">
+            <b-checkbox id="poisson" class="checkbox">
+              Poisson
+            </b-checkbox>
+          </b-field>
+          <b-field class="column is-6">
+            <b-checkbox id="cuissonPlancha" class="checkbox">
+              Cuisson à la plancha
+            </b-checkbox>
+          </b-field>
+          <b-field class="column is-6">
+            <b-checkbox id="viande" class="checkbox">
+              Viande
+            </b-checkbox>
+          </b-field>
+        </div>
+        <div class="is-flex is-justify-content-center pb-4 pt-4">
+          <b-button type="is-primary" outlined>
+            Inscription
+          </b-button>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUpItem',
+  data () {
+    return {
+      radio: 'non',
+      seen: false
+    }
+  }
+}
+
+</script>
---
+++ pages/inscription/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <div class="columns is-multiline is-justify-content-center">
+      <SignUp />
+    </div>
+  </section>
+</template>
+
+<script>
+import SignUp from '../../components/SignUp'
+export default {
+  name: 'IndexPage',
+  components: {
+    SignUp
+  }
+}
+</script>
