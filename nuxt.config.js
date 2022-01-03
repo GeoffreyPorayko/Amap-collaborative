f497790bca71dc9e0e81bd0fbc50165622dd4ae4
Kalv1 <calvin.lambert@protonmail.com>
2022-01-03 18:07:42 +0100

    :lipstick: Add animation to pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@fortawesome/fontawesome-free/css/all.css'
+    '@fortawesome/fontawesome-free/css/all.css',
+    '~/assets/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
