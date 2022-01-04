e1fcc529cd2052f5ef9c1e3a7377ba31de280c72
lucas <lucas.humbert.28@gmail.com>
2022-01-04 18:21:23 +0100

    front page contributeurs

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,45 +1,43 @@
 <template>
   <div>
-    <template>
-      <b-navbar>
-        <template #brand>
-          <b-navbar-item>
-            <img
-              src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-              alt="Lightweight UI components for Vue.js based on Bulma"
-            >
-          </b-navbar-item>
-        </template>
-        <template #start>
-          <b-navbar-item tag="nuxt-link" to="/">
-            Recette
-          </b-navbar-item>
-          <b-navbar-item tag="nuxt-link" to="/QandA">
-            Q&A
-          </b-navbar-item>
-          <b-navbar-item>
-            Producteurs
-          </b-navbar-item>
-        </template>
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item>
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          >
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item tag="nuxt-link" to="/">
+          Recette
+        </b-navbar-item>
+        <b-navbar-item tag="nuxt-link" to="/QandA">
+          Q&A
+        </b-navbar-item>
+        <b-navbar-item tag="nuxt-link" to="/producteurs">
+          Producteurs
+        </b-navbar-item>
+      </template>
 
-        <template #end>
-          <b-navbar-item tag="div">
-            <div class="buttons">
-              <Nuxt-Link to="/inscription">
-                <div class="button is-primary">
-                  <strong>Inscription</strong>
-                </div>
-              </Nuxt-Link>
-              <Nuxt-Link to="/connexion">
-                <div class="button is-light">
-                  Connexion
-                </div>
-              </Nuxt-Link>
-            </div>
-          </b-navbar-item>
-        </template>
-      </b-navbar>
-    </template>
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <Nuxt-Link to="/inscription">
+              <div class="button is-primary">
+                <strong>Inscription</strong>
+              </div>
+            </Nuxt-Link>
+            <Nuxt-Link to="/connexion">
+              <div class="button is-light">
+                Connexion
+              </div>
+            </Nuxt-Link>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
     <section class="main-content columns is-justify-content-center">
       <div class="column is-10">
         <Nuxt />
---
+++ pages/producteurs/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="section">
+    <div class="container mt-3">
+      <b-field message="Quelle producteur cherchez-vous?">
+        <b-input
+          placeholder="Rechercher..."
+          type="search"
+          icon="magnify"
+          expanded
+        />
+        <p class="control">
+          <b-button type="is-primary" label="Rechercher" />
+        </p>
+      </b-field>
+    </div>
+    <div class="columns is-multiline is-justify-content-center">
+      <CardProducteur nom="John Doe" photo="poulet.jpg" :suivi="1" />
+      <CardProducteur nom="Marcel Dupuis" photo="poulet.jpg" :suivi="0" />
+      <CardProducteur nom="John Doe" photo="poulet.jpg" :suivi="1" />
+      <CardProducteur nom="Marcel Dupuis" photo="poulet.jpg" :suivi="0" />
+      <CardProducteur nom="John Doe" photo="poulet.jpg" :suivi="0" />
+      <CardProducteur nom="Marcel Dupuis" photo="poulet.jpg" :suivi="0" />
+    </div>
+  </section>
+</template>
+
+<script>
+import CardProducteur from '../../components/CardProducteur'
+export default {
+  name: 'IndexPage',
+  components: {
+    CardProducteur
+  }
+}
+</script>
