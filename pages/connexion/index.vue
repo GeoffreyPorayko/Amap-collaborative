29b5fc8aa13fcb9a9c624ef8dd36a1ba7ed70906
lucas <lucas.humbert.28@gmail.com>
2022-01-09 00:24:56 +0100

    wip

--- pages/connexion/index.vue
+++ pages/connexion/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <div class="columns is-multiline is-justify-content-center">
-      <Connection :log-function="loginUser"/>
+      <Connection :log-function="loginUser" :erreur-email="erreurEmail" :erreur-password="erreurPassword" />
     </div>
   </section>
 </template>
@@ -16,16 +16,39 @@ export default {
   },
   data () {
     return {
-      errorMessage: ''
+      erreurEmail: '',
+      erreurPassword: ''
     }
   },
   methods: {
+    validEmail (email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+    // eslint-disable-next-line require-await
     async loginUser (loginInfo) {
-      try {
-        const response = await this.$auth.loginWith('local', { data: loginInfo })
-        console.log(response)
-      } catch (err) {
-        console.log(err)
+      this.erreurEmail = ''
+      this.erreurPassword = ''
+
+      if (!loginInfo.email) {
+        this.erreurEmail = 'Email nécessaire'
+      } else if (!this.validEmail(loginInfo.email)) {
+        this.erreurEmail = 'Email valide nécessaire'
+      }
+
+      if (!loginInfo.password) {
+        this.erreurPassword = 'Mot de passe nécessaire'
+      }
+
+      if (!this.erreurEmail.length && !this.erreurPassword.length) {
+        try {
+          const response = await this.$auth.loginWith('local', { data: loginInfo })
+          // eslint-disable-next-line no-console
+          console.log(response)
+        } catch (err) {
+          // eslint-disable-next-line no-console
+          console.log(err)
+        }
       }
     }
   }
