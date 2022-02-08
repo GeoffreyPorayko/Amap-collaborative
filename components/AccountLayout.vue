5c8bc52d997ac0e244cca4f1f9c495c1ea1bc2e1
baptistehouques <baptistehouques38@gmail.com>
2022-02-08 16:55:03 +0100

    :sparkles:profile member infos

---
+++ components/AccountLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-4 is-half mb-6">
+      <b-navbar centered style="border-radius: 5px" class="is-primary">
+        <template #start>
+          <b-navbar-item id="layout" tag="nuxt-link" to="/compte/params" class="is-size-5">
+            Compte
+          </b-navbar-item>
+          <b-navbar-item tag="nuxt-link" to="/compte/params" class="is-size-5">
+            Mes Recettes
+          </b-navbar-item>
+          <b-navbar-item tag="nuxt-link" to="/compte/params" class="is-size-5">
+            Favoris
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+<style scoped>
+
+</style>
